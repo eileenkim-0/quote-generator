de1e716e1cd1bd1d9778104c37ca3685d295c408
Eileen Kim <eileen8kim@gmail.com>
2024-11-18 21:48:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch error here
+        console.log('whoops, error here!;', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable button when clicked
+    button.disabled = true;
+    // Start picture-in-picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
+
+// await = usually used with async. 
